0a1649a4322a6b5d0c6f21205533c899de1191ef
Tim Gabets <tim@gabets.ru>
2018-03-29 09:07:44 +0300

    Dumping testcases.xml file (taken from libxml2 reader1.c example)

--- src/isoclient.cc
+++ src/isoclient.cc
@@ -6,11 +6,79 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "log.hh"
+#include <libxml/xmlreader.h>
 
 #define MAXBUFLEN 1024
 
+static void
+processNode(xmlTextReaderPtr reader)
+{
+	const xmlChar *name, *value;
+
+	name = xmlTextReaderConstName(reader);
+	if (name == NULL)
+		name = BAD_CAST "--";
+
+	value = xmlTextReaderConstValue(reader);
+
+	printf("%d %d %s %d %d",
+	       xmlTextReaderDepth(reader),
+	       xmlTextReaderNodeType(reader),
+	       name,
+	       xmlTextReaderIsEmptyElement(reader),
+	       xmlTextReaderHasValue(reader));
+	if (value == NULL)
+		printf("\n");
+	else {
+		if (xmlStrlen(value) > 40)
+			printf(" %.40s...\n", value);
+		else
+			printf(" %s\n", value);
+	}
+}
+
+/**
+ * streamFile:
+ * @filename: the file name to parse
+ *
+ * Parse and print information about an XML file.
+ */
+static void
+streamFile(const char *filename)
+{
+	xmlTextReaderPtr reader;
+	int ret;
+
+	reader = xmlReaderForFile(filename, NULL, 0);
+	if (reader != NULL) {
+		ret = xmlTextReaderRead(reader);
+		while (ret == 1) {
+			processNode(reader);
+			ret = xmlTextReaderRead(reader);
+		}
+		xmlFreeTextReader(reader);
+		if (ret != 0) {
+			fprintf(stderr, "%s : failed to parse\n", filename);
+		}
+	} else {
+		fprintf(stderr, "Unable to open %s\n", filename);
+	}
+}
+
+
 int main(int argc, char* argv[])
 {
+	// Reading XML
+	LIBXML_TEST_VERSION
+
+	streamFile(argv[1]);
+	xmlCleanupParser();
+	xmlMemoryDump();
+
+	exit(0);
+
+
+	//
 	const char* srv_name = "localhost";
 	const int port = 1337;
 
@@ -22,12 +90,12 @@ int main(int argc, char* argv[])
 	srv_addr.sin_port = htons(port);
 
 	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0){
+	if(sockfd < 0) {
 		log_error("Socket error: %s", strerror(errno));
 		return -1;
 	}
 
-	if(connect(sockfd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) < 0){
+	if(connect(sockfd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) < 0) {
 		log_error("Error connecting to %s:%d : %s", srv_name, port, strerror(errno));
 		return -1;
 	}
